7ec4f733ed55ef7b6f439e39213ad4cbf2323092
Lee-Sunho <sunho9889@soongsil.ac.kr>
2024-01-19 09:45:26 +0900

    [ADD] #6 theme 생성 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { GlobalStyle } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+
 function App() {
-  console.log('hi');
-  return <div>hello, react</div>;
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<div>hello, react</div>
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default App;
