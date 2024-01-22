1bda4e1419313aa716b96b12d1e6d37fdc10f936
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-01-22 10:15:31 +0900

    [FEAT]#9 회원가입폼 SELECT 선택 값 출력 구현

--- src/components/SignUp/GroupType.tsx
+++ src/components/SignUp/GroupType.tsx
@@ -9,15 +9,22 @@ function GroupType(){
         setIsOption(!isOption);
 	};
 
+    //그룹 분류 value값 설정하기
+    const [groupType, setGroupType] = useState("동아리");
+    const handleClickedGroupType = (type: string) => {
+        setGroupType(type);
+        setIsOption(!isOption);
+    };
+
     return(
         <div>
             <S.groupTypeBox toggle={isOption}>
-                <S.groupTypeLabel onClick={onClickLabel}>동아리</S.groupTypeLabel>
+                <S.groupTypeLabel onClick={onClickLabel}>{groupType}</S.groupTypeLabel>
                 <S.groupTypeUl toggle={isOption}>
-                    <S.groupTypeLiTop>동아리</S.groupTypeLiTop>
-                    <S.groupTypeLi>학생회</S.groupTypeLi>
-                    <S.groupTypeLi>학술 모임</S.groupTypeLi>
-                    <S.groupTypeLiBottom>기타</S.groupTypeLiBottom>
+                    <S.groupTypeLiTop onClick={(e) => handleClickedGroupType("동아리")}>동아리</S.groupTypeLiTop>
+                    <S.groupTypeLi onClick={(e) => handleClickedGroupType("학생회")}>학생회</S.groupTypeLi>
+                    <S.groupTypeLi onClick={(e) => handleClickedGroupType("학술 모임")}>학술 모임</S.groupTypeLi>
+                    <S.groupTypeLiBottom onClick={(e) => handleClickedGroupType("기타")}>기타</S.groupTypeLiBottom>
                 </S.groupTypeUl>
             </S.groupTypeBox>
         </div>
