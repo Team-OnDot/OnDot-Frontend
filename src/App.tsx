e9b48a2954205b41a539e79aacd2484ee03ff5a1
MiJeeLee <86431761+jpg723@users.noreply.github.com>
2024-01-23 10:22:05 +0900

    Revert "[FEAT] 헤더, 로그인, 회원가입 페이지 구현"

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-import Header from './pages/header/Header';
-import Login from './pages/login/Login';
-import SignUpId from './pages/signUp/SignUpId';
-import Home from './pages/home/Home';
 
 function App() {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
 				<GlobalStyle />
-				<BrowserRouter>
-					<Routes>
-						{/*헤더가 필요한 컴포넌트 */}
-						<Route element={<Header />}>
-							<Route path="/" element={<Home />} />
-							<Route path="/login" element={<Login />} />
-							<Route path="/sign-up-id" element={<SignUpId />} />
-						</Route>
-						{/*헤더가 필요없는 컴포넌트 */}
-
-					</Routes>  
-				</BrowserRouter>
+				<div>hello, react</div>
 			</ThemeProvider>
 		</>
 	);
--- src/pages/home/Home.styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const home = styled.div`
-    text-align: center;
-    margin: 100px auto;
-`;
