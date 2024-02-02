01332ed11a703bb5f31c0524e849c25590bac116
dasol729 <97594634+dasol729@users.noreply.github.com>
2024-02-02 20:09:59 +0900

    [FEAT] develop pull & merge #23

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Login from './pages/login/Login';
 import SignUpId from './pages/signUp/SignUpId';
 import Home from './pages/home/Home';
 import GroupProfile from './pages/groupProfile/GroupProfile';
+import GroupProfileSetting from './pages/groupProfile/GroupProfileSetting';
 import InterviewMake1 from './pages/interviewMake/InterviewMake1';
 import InterviewMake3 from './pages/interviewMake/InterviewMake3';
 import InterviewMakeComplete from './pages/interviewMake/InterviewMakeComplete';
@@ -20,8 +21,6 @@ import InterviewEnd from './pages/interviewApply/InterviewEnd';
 import InterviewYet from './pages/interviewApply/InterviewYet';
 import EditInfo1 from './pages/interviewApply/EditInfo1';
 import EditInfo2 from './pages/interviewApply/EditInfo2';
-
-import GroupProfileSetting from './pages/groupProfile/GroupProfileSetting';
 
 function App() {
 	return (
@@ -36,6 +35,7 @@ function App() {
 							<Route path="/login" element={<Login />} />
 							<Route path="/sign-up-id" element={<SignUpId />} />
 							<Route path="/group-profile" element={<GroupProfile />} />
+							<Route path="/group-profile-setting" element={<GroupProfileSetting />} />
 							<Route path="/interview-make-1" element={<InterviewMake1 />} />
 							<Route path="/interview-make-3" element={<InterviewMake3 />} />
 							<Route path="/interview-make-complete" element={<InterviewMakeComplete />} />
@@ -51,7 +51,6 @@ function App() {
 								<Route path="/edit-info-check" element={<EditInfo1 />} />
 								<Route path="/edit-info" element={<EditInfo2 />} />
 							</Route>
-							<Route path="/group-profile-setting" element={<GroupProfileSetting />} />
 						</Route>
 						{/*헤더가 필요없는 컴포넌트 */}
 
