52de012db9496fc314999b900bfff3ec82f80529
MiJeeLee <dlalwl723@naver.com>
2024-02-21 01:49:23 +0900

    [FEAT] #51 비밀번호 초기화 메일 전송 구현

--- src/pages/findAccount/findPassword/FindPw2.tsx
+++ src/pages/findAccount/findPassword/FindPw2.tsx
@@ -1,6 +1,7 @@
 import * as F from '../FindAccount.style';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { findEmailAtom, hashValueAtom } from '../../../recoil/findAccount';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -20,88 +21,39 @@ function FindPw2() {
         formState: { errors }
     } = useForm<IUserData>({ mode: 'onBlur' })
 
-    
-    //입력 취소 버튼
-    const removeInput = (name:any) => {
-        resetField(name);
-        setError(name,  {message: ''});
-    }
-
-    const setSendEmail = useSetRecoilState(findEmailAtom);
-    const setHashValue = useSetRecoilState(hashValueAtom);
+    const sendEmail = useRecoilValue(findEmailAtom);
+    const hashValue = useRecoilValue(hashValueAtom);
     const navigate = useNavigate();
-    const onValid = (data: IUserData) => {
 
+    //API연결(이메일 전송)
+    useEffect(() => {
         axios({
-            url: '/api/v1/auth/find/pw',
+            url: `/api/v1/auth/find/pw/verify/${sendEmail}`,
             method: 'get',
             params: {
-                email: data.email,
+                email: sendEmail,
+                code: hashValue
             },
           }).then((response) => {
             if(response.data.statusCode == "OK"){
-                setSendEmail(data.email);
-                setHashValue(response.data.content.hashValue);
+                console.log("성공");
                 //navigate("/group-profile"); //로그인 성공 시 페이지 이동
-            }
-            else{ //존재하지 않는 이메일
-                setError("email", { message: "존재하지 않는 이메일입니다." },{ shouldFocus: true });
             }
           }).catch((error) => {
             console.log("실패");  
             console.error('AxiosError:', error);
         });
-    }
+    }, []);
 
     return(
         <div>
             <F.Header>
                 <F.HeaderTitle>비밀번호 찾기</F.HeaderTitle>
-                <F.HeaderText>가입한 이메일로 비밀번호 변경 링크가 포함된 메일을 전송해 드립니다.</F.HeaderText>
+                <F.HeaderText>
+                    비밀번호 변경 링크가 포함된 이메일이 {sendEmail}으로 전송되었습니다.<br/>
+                    메일을 확인하여 비밀번호를 변경해 주세요.
+                </F.HeaderText>
             </F.Header>
-
-            <F.Main>
-                <F.FormTitle>
-                    <F.Ellipse></F.Ellipse>
-                    비밀번호 찾기
-                </F.FormTitle>
-
-                <F.FindForm onSubmit={handleSubmit(onValid)}>
-                    <F.InputBox
-                        toggle={watch("email")?.length > 0 ? true: false || errors.email ? true: false}  
-                        color={errors.email ? '#FF4A4A': '#606060'}
-                    >
-                        <F.Input
-                            id="email"
-                            type="text"
-                            placeholder="이메일을 입력해 주세요"
-                            {...register("email",{
-                                required: true,
-                                pattern: {
-                                    value: /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
-                                    message: "존재하지 않는 이메일입니다."
-                                },                            
-                            })}
-                            
-                        />
-                        {watch("email")?.length > 0 && 
-                            <F.InputCancelBtn 
-                                src={process.env.PUBLIC_URL + '/images/inputCancelIcon.svg'}
-                                onClick={e => removeInput("email")}
-                            />
-                        }
-                    </F.InputBox>
-
-                    <F.ErrorMessage>
-                        <F.ErrorText error={errors.email ? true : false}>
-                            존재하지 않는 이메일입니다.
-                        </F.ErrorText>
-                    </F.ErrorMessage>
-
-                    {/*버튼*/}
-                    <F.SubmitBtn type="submit">메일보내기</F.SubmitBtn>
-                </F.FindForm>
-            </F.Main>
         </div>
     );
 }
